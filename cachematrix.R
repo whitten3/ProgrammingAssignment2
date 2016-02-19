1dc975134e3e3240a70a08c8e308bf886d7d0c19
Doug Whitten <whitten3@msu.edu>
2016-02-19 15:06:41 -0500

    submission of programming project 2:Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming assignment 2: These functions will 
+## create a matrix and save its inverse to cache.  The
+## inverse can then be called from cache or created if 
+## it has not yet been created.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## This function will create a matrix and save its 
+## inverse to cache.
 
+makeCacheMatrix <- function(x=matrix()){
+  x<-matrix(x, nrow=2, ncol=2) 
+  
+  m<-NULL
+  set<-function(y){
+    x<<-y
+    m<<-NULL
+  }
+  get<-function()x
+  setinv<-function(solve) m<<-solve
+  getinv<-function(y) m
+  list( set=set, get=get,
+        setinv=setinv, getinv=getinv)
+  
+  
 }
 
+ ## If the inverted matrix exists, this function 
+## will return that inversion.  If not, it will 
+## create and return the invertesion.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve<-function(x, ...){
+  m<-x$getinv()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  data<-x$get()
+  m<-solve(data, ...)
+  x$setinv(m)
+  m
 }
+
